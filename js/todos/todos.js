24ea0b60c863ab7910cabf4fe46319d26000acbf
Dan Prowse <danprowse@hotmail.co.uk>
2021-07-11 14:04:03 +0100

    todo can be added and is displayed

--- js/todos/todos.js
+++ js/todos/todos.js
@@ -1,20 +1,23 @@
-const todoInput = document.querySelector('.todos__input');
-const todoError = document.querySelector('.todos__error');
-const todoList = document.querySelector('.todos__list');
+const todoInput = document.querySelector(".todos__input");
+const todoError = document.querySelector(".todos__error");
+const todoList = document.querySelector(".todos__list");
 let currentTodos = [];
 export class todos {
-    constructor() {
-
+  constructor() {
+  }
+  addTodo() {
+    if (todoInput.value.length == 0) {
+      todoError.textContent = "Input is empty";
+    } else {
+      todoError.textContent = "";
+      // TODO: check user input
+      let todo = todoInput.value;
+      currentTodos.push(todo);
+      const li = document.createElement("li");
+      li.appendChild(document.createTextNode(todo));
+      todoList.appendChild(li);
     }
-    addTodo() {
-        if(todoInput.value.length == 0) {
-            todoError.textContent = 'Input is empty'
-        }
-    }
-    deleteTodo() {
-
-    }
-    toggleCompleteTodo() {
-
-    }
+  }
+  deleteTodo() {}
+  toggleCompleteTodo() {}
 }
