206bac9681b5cd993c4f9a03e1af59cc794fd6af
Dan Prowse <danprowse@hotmail.co.uk>
2021-07-15 01:13:00 +0100

    removed old todo files

--- js/todos/todo-list-component.js
+++
@@ -1,7 +0,0 @@
-export function listComponent(todo) {
-    return `<li>
-                ${todo}
-                <i class="fas fa-check complete"></i>
-                <i class="fas fa-times delete"></i>
-            </li>`
-}
